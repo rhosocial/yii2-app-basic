13aecde4f975e860121e1f929328897a026e4013
Zhao vistart <i@vistart.vip>
2017-04-02 20:43:02 +0800

    update about.

--- views/site/about.php
+++ views/site/about.php
@@ -15,4 +15,48 @@ $this->params['breadcrumbs'][] = $this->title;
     </p>
 
     <code><?= __FILE__ ?></code>
+<?php
+$markdown = <<<EOT
+# Installation
+
+## Database & Connection
+
+Create database & change database connection configuration.
+
+## Migration
+
+You should execute the following migrations:
+
+Create `user`, `profile`, `password_history` tables:
+
+```
+yii migrate --migrationPath=@rhosocial/user --migrationNamespace=rhosocial\user\migrations;
+```
+
+Create authorization tables:
+
+```
+yii migrate --migrationPath=@rhosocial/user --migrationNamespace=rhosocial\user\\rbac\migrations;
+```
+
+Create `login_log` table:
+
+```
+yii migrate --migrationPath=@rhosocial/user --migrationNamespace=rhosocial\user\models\log\migrations;
+```
+
+Create organization and it's profile and member tables:
+
+```
+yii migrate --migrationPath=@rhosocial/organization --migrationNamespace=rhosocial\organization\migrations;
+```
+
+Create organization authorization tables:
+
+```
+yii migrate --migrationPath=@rhosocial/organization --migrationNamespace=rhosocial\organization\\rbac\migrations;
+```
+EOT;
+echo (new \cebe\markdown\GithubMarkdown())->parse($markdown);
+?>
 </div>
